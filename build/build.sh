b5761a37c6751a710db4e3a41acd7d14238023e1
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2021-01-05 22:18:06 +0530

    add build scripts

---
+++ build/build.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+## read build mode
+
+# by default dev mode
+buildMode=0
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		-d|--dev) buildMode=0; shift ;;
+		-p|--production) buildMode=1;;
+		*) echo "[ERR] Unknown build option: $1"; exit 1 ;;
+	esac
+	shift
+done
+
+## functions
+buildMetapodBase ()
+{
+	docker build -t="metapod/base" -f ./build/Dockerfile .
+}
+
+## Check if Docker exists (on Ubuntu bash)
+dockerCheck=$(which docker)
+if [ $? -ne 0 ]
+then
+	echo "[ERR] Docker not installed"
+	exit 1
+else
+	echo "[INFO] Docker installed"
+fi
+
+## Check if metapod/base already exists on system
+imageCheck=$(docker image ls | grep 'metapod/base')
+if [ $? -ne 0 ]
+then
+	echo "[INFO] metapod/base doesn't exist -> will build from scratch"
+	echo "[INFO] Starting metapod/base build process"
+	buildMetapodBase
+else
+	echo "[INFO] metapod/base exists"
+fi
+
+## Perform appropriate build
+# dev mode
+if [ $buildMode -eq 0 ]
+then 
+	echo "[INFO] DEV mode activated"
+	checkContainerExists=$(docker container ls -a | grep 'metapod')
+	if [ $? -eq 0 ]; then
+		checkContainerRunning=$(docker container ls | grep 'metapod')
+		if [ $? -eq 0 ]; then
+			echo "[INFO] Container running. Will stop it."
+			docker stop metapod-dev
+		fi
+		echo "[INFO] Container exists. Will delete it."
+		docker rm metapod-dev
+	fi
+	echo "[INFO] Begin container launch."
+	docker run -v $(pwd):/src -v "/var/run/docker.sock:/var/run/docker.sock:rw" -p 6969:3000 -p 4200:4000 --name=metapod-dev metapod/base
+else
+	echo "[ERR] Production mode not supported"
+fi
