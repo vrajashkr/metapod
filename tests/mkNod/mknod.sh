e1c115ea24b6af4f66d3ec35487c02ac357104c4
saiprashanth1776 <prashanth.sai99@gmail.com>
2021-02-28 19:38:01 +0530

    chnages to the test scripts

--- tests/mkNod/mknod.sh
+++ tests/mkNod/mknod.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Creating a device"
-mknod /dev/fd2 c 1 8
+echo "running command mknod /dev/fd2 c 1 8"
+`mknod /dev/fd2 c 1 8`
+if [ $? -eq 0 ];
+then
+	echo "Device created successfully!"
+elif [ $? -eq -1 ];
+then 
+	echo "Device creation failed!"
+fi
